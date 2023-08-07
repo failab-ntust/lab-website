2079dc40091192ffecdc1f32166ac99ac7a0cd6e
Tiffany0313 <pinky95123@gmail.com>
2023-08-07 17:46:33 +0800

    feat: add professor course

--- app/professor/Information.js
+++ app/professor/Information.js
@@ -1,6 +1,7 @@
 'use client';
 import * as React from 'react';
-import { Box, Typography, ListItem, Stack, Divider } from '@mui/material';
+import PropTypes from 'prop-types';
+import { Box, Typography, ListItem, Stack, Divider, Grid, Tabs, Tab, Link } from '@mui/material';
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 import CircleIcon from '@mui/icons-material/Circle';
 import Image from 'next/image'
@@ -19,7 +20,6 @@ const CusBox = ({ title, children }) => (
     </>
 )
 
-
 const Dots = () => (
     <Stack direction="row" spacing={2} justifyContent="center" m={3} mt={6} >
         {Array.from(Array(3)).map((_, index) => (
@@ -36,6 +36,17 @@ const Education = ({ school, degree, duringtime }) => (
         <Stack direction="row" spacing={2}>
             <Stack direction="row" justifyContent="center" alignItems="center">
                 <Image
+                    src='date.svg'
+                    alt='date'
+                    width={23}
+                    height={23}
+                />
+                <Typography sx={{ bgcolor: '#FFD700', ml: 1 }}>
+                    {duringtime}
+                </Typography>
+            </Stack>
+            <Stack direction="row" justifyContent="center" alignItems="center">
+                <Image
                     src='degree.svg'
                     alt='degree'
                     width={30}
@@ -45,11 +56,7 @@ const Education = ({ school, degree, duringtime }) => (
                     {degree}
                 </Typography>
             </Stack>
-            <Stack justifyContent="center">
-                <Typography sx={{ bgcolor: '#FFD700', px: 1 }}>
-                    {duringtime}
-                </Typography>
-            </Stack>
+
         </Stack>
     </Stack>
 )
@@ -59,8 +66,19 @@ const Experience = ({ workplace, position, duringtime }) => (
         <Typography variant='h6'>
             {workplace}
         </Typography>
-        <Stack direction="row" spacing={2}>
-            <Stack direction="row" justifyContent="center" alignItems="center">
+        <Stack direction={{ xs: 'column', sm: 'row' }} spacing={{ xs: 0.5, sm: 2 }}>
+            <Stack direction="row" justifyContent={{ xs: 'flex-start', sm: 'center' }} alignItems="center">
+                <Image
+                    src='date.svg'
+                    alt='date'
+                    width={23}
+                    height={23}
+                />
+                <Typography component='span' sx={{ bgcolor: '#FFD700', width: 'fit-content', ml: 1 }}>
+                    {duringtime}
+                </Typography>
+            </Stack>
+            <Stack direction="row" justifyContent={{ xs: 'flex-start', sm: 'center' }} alignItems="center">
                 <Image
                     src='job-position.svg'
                     alt='job-position'
@@ -71,16 +89,87 @@ const Experience = ({ workplace, position, duringtime }) => (
                     {position}
                 </Typography>
             </Stack>
-            <Stack justifyContent="center">
-                <Typography sx={{ bgcolor: '#FFD700', px: 1 }}>
-                    {duringtime}
-                </Typography>
-            </Stack>
         </Stack>
-    </Stack>
+    </Stack >
 )
 
+const MBACourses = [
+    { course: '數位金融概論111-1', link: 'https://moodle2.ntust.edu.tw/course/view.php?id=4405', img: 'digital-finance' }
+]
+const UniversityCourses = [
+    { course: '大數據分析導論 111-1', link: 'https://moodle2.ntust.edu.tw/course/view.php?id=4005', img: 'big-data' },
+    { course: '大數據分析導論 110-2', link: 'https://moodle2.ntust.edu.tw/course/view.php?id=1790', img: 'big-data' },
+    { course: '計算機概論 111-2', link: 'https://moodle2.ntust.edu.tw/course/view.php?id=5832', img: 'computer-science' }
+]
+const CourseGrid = ({ course, link, img }) => (
+    <Stack direction="column" justifyContent="center" alignItems="center"
+        spacing={2}
+        component={Link}
+        href={link}
+        target="_blank"
+        sx={{
+            textDecoration: 'none',
+            color: 'inherit',
+            bgcolor: 'white',
+            height: { xs: 200, sm: 250 },
+            transition: "transform 200ms ease-in-out",
+            "&:hover": {
+                cursor: "pointer",
+                boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+                backgroundColor: "white",
+                transform: "scale(1.02)"
+            }
+        }}>
+        <Image
+            src={`${img}.svg`}
+            alt={img}
+            width={100}
+            height={100}
+        />
+        <Typography variant='body1' align="center" sx={{ fontWeight: 600 }}>
+            {course}
+        </Typography>
+    </Stack>
+)
+function CustomTabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Grid container spacing={{ xs: 2, md: 3, lg: 4 }} columns={{ xs: 4, sm: 9, md: 12, lg: 16 }} >
+                    {children}
+                </Grid>
+            )}
+        </div>
+    );
+}
+
+CustomTabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
 export default function Information() {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     return (
         <>
@@ -96,38 +185,67 @@ export default function Information() {
 
             <Dots />
 
-            <CusBox title="學歷">
-                <Education {...{ school: '國立成功大學工業與資訊管理所', degree: '博士', duringtime: '2009/09～2012/05' }} />
+            <Grid container spacing={{ xs: 2, md: 3, lg: 4 }} columns={{ xs: 4, sm: 8, md: 12 }} >
+                <Grid item xs={4} sm={8} md={6}>
+                    <CusBox title="經歷">
 
-                <Divider sx={{ m: 2 }} />
+                        <Experience {...{ workplace: '國立台北商業大學 – 會計資訊系', position: '專案助理教授', duringtime: '2021/08～2022/07' }} />
 
-                <Education {...{ school: '國立中正大學醫療與資訊管理所', degree: '碩士', duringtime: '2007/09～2009/07' }} />
-            </CusBox>
+                        <Divider sx={{ m: 2 }} />
 
-            <Dots />
+                        <Experience {...{ workplace: '台灣智慧財產運營協會', position: '秘書長', duringtime: '2022/01～2022/07' }} />
 
-            <CusBox title="經歷">
+                        <Divider sx={{ m: 2 }} />
 
-                <Experience {...{ workplace: '國立台北商業大學 – 會計資訊系', position: '專案助理教授', duringtime: '2021/08～2022/07' }} />
+                        <Experience {...{ workplace: '富邦媒體科技 – 資訊科技處', position: '專案經理', duringtime: '2014/09～2021/07' }} />
 
-                <Divider sx={{ m: 2 }} />
+                        <Divider sx={{ m: 2 }} />
 
-                <Experience {...{ workplace: '台灣智慧財產運營協會', position: '秘書長', duringtime: '2022/01～2022/07' }} />
+                        <Experience {...{ workplace: '富邦金控 – 創新科技辦公室', position: '大數據暨人工智慧組組長', duringtime: '2015/10～2020/08' }} />
+                    </CusBox>
+                </Grid>
+                <Grid item xs={4} sm={8} md={6}>
+                    <CusBox title="學歷">
+                        <Education {...{ school: '國立成功大學工業與資訊管理所', degree: '博士', duringtime: '2009/09～2012/05' }} />
 
-                <Divider sx={{ m: 2 }} />
+                        <Divider sx={{ m: 2 }} />
 
-                <Experience {...{ workplace: '富邦媒體科技 – 資訊科技處', position: '專案經理', duringtime: '2014/09～2021/07' }} />
+                        <Education {...{ school: '國立中正大學醫療與資訊管理所', degree: '碩士', duringtime: '2007/09～2009/07' }} />
+                    </CusBox>
 
-                <Divider sx={{ m: 2 }} />
-
-                <Experience {...{ workplace: '富邦金控 – 創新科技辦公室', position: '大數據暨人工智慧組組長', duringtime: '2015/10～2020/08' }} />
-            </CusBox>
-
+                </Grid>
+            </Grid>
             <Dots />
 
             <CusBox title="教授課程">
+                <Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 3 }}>
+                    <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        aria-label="basic tabs example"
+                        textColor='secondary'
+                        indicatorColor='secondary'
+                    >
+                        <Tab label="MBA" disableRipple {...a11yProps(0)} />
+                        <Tab label="大學部" disableRipple {...a11yProps(1)} />
+                    </Tabs>
+                </Box>
 
-            </CusBox>
+                <CustomTabPanel value={value} index={0}>
+                    {MBACourses.map((mba, index) => (
+                        <Grid item xs={4} sm={3} md={4} lg={4} key={index}>
+                            <CourseGrid course={mba.course} link={mba.link} img={mba.img} />
+                        </Grid>
+                    ))}
+                </CustomTabPanel>
+                <CustomTabPanel value={value} index={1}>
+                    {UniversityCourses.map((university, index) => (
+                        <Grid item xs={4} sm={3} md={4} lg={4} key={index}>
+                            <CourseGrid course={university.course} link={university.link} img={university.img} />
+                        </Grid>
+                    ))}
+                </CustomTabPanel>
+            </CusBox >
         </>
     );
 }
