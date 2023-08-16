7137991dd35764b9c75b97814575c2de2db612d4
Tiffany0313 <pinky95123@gmail.com>
2023-08-16 16:04:00 +0800

    feat: add opportunity layout

--- app/opportunity/page.tsx
+++ app/opportunity/page.tsx
@@ -4,21 +4,22 @@ import { Box } from '@mui/material';
 
 import PageTitle from '../PageTitle';
 import SubTitle from '../SubTitle'
+import ControlledAccordions from './ControlledAccordions'
 
 function Opportunity() {
     return (
         <Box>
             <PageTitle title='業界機會' />
             <SubTitle title='專案行銷類'>
-
+                <ControlledAccordions />
             </SubTitle>
             <SubTitle title='數據AI類'>
-
+                <ControlledAccordions />
             </SubTitle>
             <SubTitle title='開發實作類'>
-
+                <ControlledAccordions />
             </SubTitle>
-        </Box>
+        </Box >
 
     )
 }
