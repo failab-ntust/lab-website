4f5a1d237929da200edfc6b0a63c9965732122ca
Tiffany0313 <pinky95123@gmail.com>
2023-08-21 17:37:14 +0800

    feat: add QR code

--- app/professor/About.js
+++ app/professor/About.js
@@ -25,59 +25,65 @@ function About() {
                 <Typography variant="h6" sx={{ lineHeight: 'normal', fontWeight: 600, px: 1 }} align='center'>
                     管理學士班/資管所甲組/人工智慧所 助理教授
                 </Typography>
-                <Box sx={{ bgcolor: { xs: 'transparent', md: 'white' }, p: 1.5 }}>
-                    <ListItem disableGutters>
-                        <ListItemAvatar>
-                            <Tooltip title="Email" placement="left">
-                                <Avatar sx={{ bgcolor: 'primary' }} variant="circular">
-                                    <EmailIcon />
-                                </Avatar>
-                            </Tooltip>
-                        </ListItemAvatar>
-                        <ListItemText primary={
-                            <Stack direction="row">
+                <Stack direction={{ xs: 'column', md: 'row' }} justifyContent='center' alignItems='center' spacing={2}>
+                    <Box sx={{ bgcolor: { xs: 'transparent', md: 'white' }, p: 1.5 }}>
+                        <ListItem disableGutters>
+                            <ListItemAvatar>
+                                <Tooltip title="Email" placement="left">
+                                    <Avatar sx={{ bgcolor: 'primary' }} variant="circular">
+                                        <EmailIcon />
+                                    </Avatar>
+                                </Tooltip>
+                            </ListItemAvatar>
+                            <ListItemText primary={
+                                <Stack direction="row">
+                                    <Typography variant="body1" >
+                                        Mail：
+                                    </Typography>
+                                    <Typography component={Link} variant="body1" color='secondary' href='mailto:ccchang@mail.ntust.edu.tw' sx={{ textDecoration: 'underline' }}>
+                                        ccchang@mail.ntust.edu.tw
+                                    </Typography>
+                                </Stack>
+                            } />
+                        </ListItem>
+
+                        <ListItem disableGutters>
+                            <ListItemAvatar>
+                                <Tooltip title="Office" placement="left">
+                                    <Avatar sx={{ bgcolor: 'primary' }} variant="circular">
+                                        <BusinessIcon />
+                                    </Avatar>
+                                </Tooltip>
+                            </ListItemAvatar>
+                            <ListItemText primary={
+                                <Typography variant="body1">
+                                    Office： MA-205-4
+                                </Typography>
+                            } />
+                        </ListItem>
+
+                        <ListItem disableGutters>
+                            <ListItemAvatar>
+                                <Tooltip title="Phone" placement="left">
+                                    <Avatar sx={{ bgcolor: 'primary' }} variant="circular">
+                                        <SmartphoneIcon />
+                                    </Avatar>
+                                </Tooltip>
+                            </ListItemAvatar>
+                            <ListItemText primary={
                                 <Typography variant="body1" >
-                                    Mail：
+                                    Phone：+886-2737-6598
                                 </Typography>
-                                <Typography component={Link} variant="body1" color='secondary' href='mailto:ccchang@mail.ntust.edu.tw' sx={{ textDecoration: 'underline' }}>
-                                    ccchang@mail.ntust.edu.tw
-                                </Typography>
-                            </Stack>
-                        } />
-                    </ListItem>
-
-                    <ListItem disableGutters>
-                        <ListItemAvatar>
-                            <Tooltip title="Office" placement="left">
-                                <Avatar sx={{ bgcolor: 'primary' }} variant="circular">
-                                    <BusinessIcon />
-                                </Avatar>
-                            </Tooltip>
-                        </ListItemAvatar>
-                        <ListItemText primary={
-                            <Typography variant="body1">
-                                Office： MA-205-4
-                            </Typography>
-                        } />
-                    </ListItem>
-
-                    <ListItem disableGutters>
-                        <ListItemAvatar>
-                            <Tooltip title="Phone" placement="left">
-                                <Avatar sx={{ bgcolor: 'primary' }} variant="circular">
-                                    <SmartphoneIcon />
-                                </Avatar>
-                            </Tooltip>
-                        </ListItemAvatar>
-                        <ListItemText primary={
-                            <Typography variant="body1" >
-                                Phone：+886-2737-6598
-                            </Typography>
-                        } />
-                    </ListItem>
-                </Box>
+                            } />
+                        </ListItem>
+                    </Box>
+                    <Stack direction='column'>
+                        <Image src='/QR code.jpg' alt='QR code' width={100} height={100} />
+                        <Typography>歡迎與我聯繫</Typography>
+                    </Stack>
+                </Stack>
             </Stack>
-        </Stack>
+        </Stack >
     )
 }
 
