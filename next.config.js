7122966ffcb425f305c2b122bdc19006de64f888
Tiffany0313 <pinky95123@gmail.com>
2024-10-29 16:50:50 +0800

    fix: config setting

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    reactStrictMode: true,
-    assetPrefix: 'https://Tiffany0313.github.io/lab-website/'
+    basePath: '/lab-website',
 }
 
 module.exports = nextConfig
