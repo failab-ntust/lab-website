094151580cf87cd05316b32ff82eb02baf71ec0b
Tiffany0313 <pinky95123@gmail.com>
2024-10-29 16:45:46 +0800

    feat: add config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+    assetPrefix: 'https://Tiffany0313.github.io/lab-website/'
+}
 
 module.exports = nextConfig
