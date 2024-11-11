ccf871adb42660f23224aabcf9090076614aa66b
Tiffany0313 <pinky95123@gmail.com>
2024-11-11 21:35:28 +0800

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    trailingSlash: true,  // 用於生成靜態頁面時，在每個 URL 末尾加上斜線（例如：/about/ 而不是 /about）
-    images: {
-        unoptimized: true,  // 禁用 Next.js 預設的圖片優化，因為 GitHub Pages 不支持
-    },
+    reactStrictMode: true,
+    assetPrefix: 'https://tiffany0313.github.io/lab-website/'
 }
 
 module.exports = nextConfig
