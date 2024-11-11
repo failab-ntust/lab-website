2ccf0093f93b6c56db91d89d32e9e9aea1ae64e3
Tiffany0313 <pinky95123@gmail.com>
2024-11-11 19:32:52 +0800

    fix config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     trailingSlash: true,  // 用於生成靜態頁面時，在每個 URL 末尾加上斜線（例如：/about/ 而不是 /about）
     images: {
         unoptimized: true,  // 禁用 Next.js 預設的圖片優化，因為 GitHub Pages 不支持
