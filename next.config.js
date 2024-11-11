def3f8a0d9219f03ae4954c35943f519c7af736e
Tiffany0313 <pinky95123@gmail.com>
2024-11-11 22:40:29 +0800

    test

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
     output: 'export',
     reactStrictMode: true,
+    exportPathMap: function () { // /Next-React-Components
+        return {
+            "/": { page: "/" },
+            "/professor": { page: "/professor" },
+            "/research": { page: "/research" },
+            "/research_result": { page: "/research_result" },
+            "/members": { page: "/members" },
+            "/graduate_members": { page: "/graduate_members" },
+        }
+    },
     assetPrefix: 'https://tiffany0313.github.io/lab-website/'
 }
 
