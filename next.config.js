ac0c8970100492fdde7ce454489b8ad5e4809cad
Tiffany0313 <pinky95123@gmail.com>
2024-11-11 23:01:32 +0800

    test

--- next.config.js
+++ next.config.js
@@ -2,17 +2,7 @@
 const nextConfig = {
     output: 'export',
     reactStrictMode: true,
-    exportPathMap: async function () {
-        return {
-            '/': { page: '/' },
-            '/professor': { page: '/professor' },
-            '/research': { page: '/research' },
-            '/research_result': { page: '/research_result' },
-            '/members': { page: '/members' },
-            '/graduate_members': { page: '/graduate_members' },
-        };
-    },
-    assetPrefix: 'https://tiffany0313.github.io/lab-website/',
+    assetPrefix: 'https://tiffany0313.github.io/lab-website/'
 };
 
 module.exports = nextConfig;
