8512083b5d7732bbfc72a320e5d28b22c4c2f4d4
Tiffany0313 <pinky95123@gmail.com>
2024-11-11 19:23:38 +0800

    add yml

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    trailingSlash: true,  // 用於生成靜態頁面時，在每個 URL 末尾加上斜線（例如：/about/ 而不是 /about）
+    images: {
+        unoptimized: true,  // 禁用 Next.js 預設的圖片優化，因為 GitHub Pages 不支持
+    },
+}
 
 module.exports = nextConfig
