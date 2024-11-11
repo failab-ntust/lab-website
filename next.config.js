409523ae27e80abd6f1e2dd0ab3bb0dacec78232
Tiffany0313 <pinky95123@gmail.com>
2024-11-11 22:20:50 +0800

    test

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: 'export',
     reactStrictMode: true,
-    assetPrefix: 'https://tiffany0313.github.io/'
+    assetPrefix: 'https://tiffany0313.github.io/lab-website/'
 }
 
 module.exports = nextConfig
