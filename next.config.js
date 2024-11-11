a5aa23dbbab9ac5726a334a31b442db46d439d01
Tiffany0313 <pinky95123@gmail.com>
2024-11-11 23:14:28 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    assetPrefix: 'https://tiffany0313.github.io/lab-website/'
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
