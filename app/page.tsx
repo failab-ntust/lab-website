8240577170e59eac38a983e43f3dd56eee4cd072
Tiffany0313 <pinky95123@gmail.com>
2025-03-05 12:48:12 +0800

    update homepage text size

--- app/page.tsx
+++ app/page.tsx
@@ -21,11 +21,11 @@ const StickyAnnouncement = () => (
   <Stack>
     <Stack direction='row' alignItems='baseline' spacing={{ xs: .5, sm: 1 }}>
       <Image src='/fire.gif' alt='fire' width={30} height={30} />
-      <Typography sx={{ typography: { xs: 'h6', sm: 'h5' } }}>實驗室成員募集中，歡迎資管所甲組、人工智慧所及管理研究所碩博士同學一起開疆闢土</Typography>
+      <Typography sx={{ typography: { xs: 'h6', sm: 'h5', md: 'h6', lg: 'h5' } }}>實驗室成員募集中，歡迎資管所甲組、人工智慧所及管理研究所碩博士同學一起開疆闢土</Typography>
     </Stack>
     <Stack direction='row' alignItems='baseline' spacing={{ xs: .5, sm: 1 }}>
       <Image src='/fire.gif' alt='fire' width={30} height={30} />
-      <Typography sx={{ typography: { xs: 'h6', sm: 'h5' } }}>歡迎各系所大學部同學參加專題及競賽</Typography>
+      <Typography sx={{ typography: { xs: 'h6', sm: 'h5', md: 'h6', lg: 'h5' } }}>歡迎各系所大學部同學參加專題及競賽</Typography>
     </Stack>
   </Stack>
 )
@@ -37,13 +37,13 @@ export default function Home() {
         <Grid item xs={4} sm={8} md={12}>
           <Box sx={{ display: 'flex', justifyContent: { xs: 'space-around', md: 'flex-start' } }}>
             <Stack justifyContent='space-evenly'>
-              <Typography sx={{ typography: { xs: 'h6', sm: 'h5' }, color: '#777777' }}>
+              <Typography sx={{ typography: { xs: 'h6', sm: 'h5', md: 'h6', lg: 'h5' }, color: '#777777' }}>
                 臺灣科技大學
               </Typography>
-              <Typography sx={{ typography: { xs: { fontSize: 28 }, sm: 'h3', fontWeight: '700 !important' } }}>
+              <Typography sx={{ typography: { xs: { fontSize: 28 }, sm: 'h3', md: { fontSize: 45 }, lg: 'h3', fontWeight: '700 !important' } }}>
                 金融科技與人工智慧實驗室
               </Typography>
-              <Typography sx={{ typography: { xs: 'h6', sm: 'h5' } }}>
+              <Typography sx={{ typography: { xs: 'h6', sm: 'h5', md: 'h6', lg: 'h5' } }}>
                 Fintech and Artificial Intelligence Lab
               </Typography>
               <br />
