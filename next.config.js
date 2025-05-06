554bc6298a98716070918d5f17fb05ee7f6468f4
Tiffany0313 <pinky95123@gmail.com>
2025-05-07 00:23:12 +0800

    update deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    trailingSlash: true,
+  };
+  
+  module.exports = nextConfig;
+  
