4a3668cd1e9aaaf7ff7e6d99c6fe868753e7323f
Tiffany0313 <pinky95123@gmail.com>
2025-05-07 11:46:47 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    reactStrictMode: true,
     images: {
         unoptimized: true,
     },
     basePath: '/lab-website',
-    assetPrefix: '/lab-website',
+    assetPrefix: 'https://failab-ntust.github.io/lab-website/',
     trailingSlash: true,
 };
 
