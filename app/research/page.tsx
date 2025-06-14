274387284f608a422a0893c92096c3ff4c46ba19
dashubigtree <91452321+dashubigtree@users.noreply.github.com>
2025-06-14 15:36:14 +0800

    update research description

--- app/research/page.tsx
+++ app/research/page.tsx
@@ -34,7 +34,7 @@ const FieldContent = ({ icon, subfield, subcontent }: any) => (
 
 export default function Research() {
     return (
-        <Box>
+         <Box>
             <PageTitle title='研究領域' />
             <Typography variant='h6' >
                 FAILab 主要研究方向以行為理論、資料科學及機器學習前瞻技術應用於資訊安全、金融產業、運動產業及企業數位轉型。
@@ -43,28 +43,28 @@ export default function Research() {
             <br />
             <Grid container spacing={3} columns={{ xs: 4, sm: 8, md: 12 }} >
                 <Grid size={{ xs: 4, sm: 8, md: 6 }} >
-                    <FieldGrid field="數位轉型" >
-                        <FieldContent icon={<Looks1Icon color='primary' />} subfield='數據賦能' subcontent='在大數據時代下，數據除了蒐集紀錄相關資訊外也同時隱含其他潛在可能的資訊。實驗室將協助企業透過視覺化軟體及No Code AI工具輔助提升企業在數據運用的價值。' />
-                        <FieldContent icon={<Looks2Icon color='primary' />} subfield='流程自動化及優化' subcontent='企業仍存在過多人工及反覆行為，可透過RPA工具（Robotic Process Automation）協助企業進行流程自訂化，並運用AI進行流程監控及優化。' />
+                    <FieldGrid field="生成式AI／大型語言模型" >
+                        <FieldContent icon={<Looks1Icon color='primary' />} subfield='多模態LLM' subcontent='讓AI能同時理解文字、圖片、影片與聲音，全面擴展其理解能力。' />
+                        <FieldContent icon={<Looks2Icon color='primary' />} subfield='全流程自動化合規 agent' subcontent='運用AI的思考與記憶能力，實現企業跨部門因應法規變動的全流程自動化合規，進一步提升員工對合規性的重視與執行力。' />
+                        <FieldContent icon={<Looks3Icon color='primary' />} subfield='模型效能自動化學習' subcontent='自動進行模型資源最佳化及績效追蹤，簡化模型迭代所需要耗費的人力。' />
                     </FieldGrid>
                 </Grid>
                 <Grid size={{ xs: 4, sm: 8, md: 6 }}>
-                    <FieldGrid field="金融科技" >
-                        <FieldContent icon={<Looks1Icon color='primary' />} subfield='數據整合' subcontent='解決跨產業資料使用困境，在保護隱私機制下建構可分析的跨產業資料。' />
-                        <FieldContent icon={<Looks2Icon color='primary' />} subfield='單一登入機制驗證' subcontent='建構金控層跨單位單一登入機制，並基於不同風險設計驗證機制。' />
-                        <FieldContent icon={<Looks3Icon color='primary' />} subfield='大數據及人工智慧應用' subcontent='建構客戶單一視圖，基於客戶行為運用人工智慧方式進行預測，可應用於異常偵測及Next Action。' />
+                    <FieldGrid field="數位轉型及數據模型治理" >
+                        <FieldContent icon={<Looks1Icon color='primary' />} subfield='數據整合及治理' subcontent='著重於資料的整合、管理與品質提升，建立企業內部及跨部門的數據治理機制。' />
+                        <FieldContent icon={<Looks2Icon color='primary' />} subfield='模型生態系' subcontent='建構協同模型訓練架構，針對不同特性的資料發展專屬AI模型，並在應用階段進行模型合併與異質數據知識整合。' />
                     </FieldGrid>
                 </Grid>
                 <Grid size={{ xs: 4, sm: 8, md: 6 }}>
-                    <FieldGrid field="運動科技 - 羽球" >
-                        <FieldContent icon={<Looks1Icon color='primary' />} subfield='使用者軟體設計' subcontent='建構以客戶為體驗之羽球APP。' />
-                        <FieldContent icon={<Looks2Icon color='primary' />} subfield='訓練優化' subcontent='透過影像辨識及人工智慧技術分析球員體態及策略應用。' />
+                    <FieldGrid field="資訊共享及隱私保護" >
+                        <FieldContent icon={<Looks1Icon color='primary' />} subfield='使用者行為偵測' subcontent='透過分散於各產業的客戶資料，應用AI於客戶與員工行為監控，及早發現異常活動，防範詐騙及資料外洩，提升資訊安全。' />
+                        <FieldContent icon={<Looks2Icon color='primary' />} subfield='安全且合規的資訊共享模式' subcontent='聚焦於企業間或跨產業資料流通時的隱私保護與法規遵循，確保資訊共享過程中資料安全與合規。' />
                     </FieldGrid>
                 </Grid>
                 <Grid size={{ xs: 4, sm: 8, md: 6 }}>
-                    <FieldGrid field="資訊安全">
-                        <FieldContent icon={<Looks1Icon color='primary' />} subfield='保護隱私框架建立' subcontent='在保護個人隱私下建構可分析式資料平台。' />
-                        <FieldContent icon={<Looks2Icon color='primary' />} subfield='使用者行為偵測' subcontent='運用人工智慧建構單一使用者行為態樣，判斷並偵測使用者異常行為。' />
+                    <FieldGrid field="圖探勘／學習及深度學習">
+                        <FieldContent icon={<Looks1Icon color='primary' />} subfield='跨產業資料應用' subcontent='運用圖探勘技術提升客戶資訊散落在不同產業間的連結，洞悉多面向客戶行為或信用評等。' />
+                        <FieldContent icon={<Looks2Icon color='primary' />} subfield='深度學習' subcontent='將深度學習技術應用於異常偵測、行為預測及決策支援等場景，提升模型準確度與智能化程度。' />
                     </FieldGrid>
                 </Grid>
             </Grid>
